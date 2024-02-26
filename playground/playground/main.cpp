22ad99b905d344b50d50df1d7482703eedbe69ae
samuelowino <samuelowino43@yahoo.com>
2024-02-26 18:33:59 +0300

    Init

---
+++ playground/playground/main.cpp
@@ -0,0 +1,11 @@
+//
+//  main.cpp
+//  playground
+//
+//  Created by Samuel Owino on 26/02/2024.
+//
+#include <iostream>
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
